c9bc95f7a10b5b05ee7900f27b66522a6b0878f2
劉康民 <gugod@gugod.org>
2005-04-06 02:08:35 +0000

    Import of GUGOD/Acme-NumericMethod-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-NumericMethod
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GUGOD/Acme-NumericMethod-0.01.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/NumericMethod.pm
@@ -0,0 +1,33 @@
+package Acme::NumericMethod;
+our $VERSION='0.01';
+package main;
+use Lingua::EN::WordsToNumbers;
+sub AUTOLOAD {
+    my $n = $AUTOLOAD;
+    $n =~ s/.*:://;
+    my $number = eval 'Lingua::EN::WordsToNumbers->new->parse($n)';
+    $@?0:$number;
+}
+1;
+__END__
+
+=head1 NAME
+
+  Acme::NumericMethod - I know numeric methods
+
+=head1 SYNOPSIS
+
+  use Acme::NumericMethod;
+  print one(); # 1
+
+=head1 COPYRIGHT
+
+Copyright 2005 by Kang-min Liu <gugod@gugod.org>.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+See <http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
+
